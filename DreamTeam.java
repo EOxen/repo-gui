25960ea3fc9be8735a4e813cd4bbddb6025b5b81
eoxen <e.oxenhorn@gmail.com>
2021-10-17 19:38:22 +0300

    Created DreamTeam.java

---
+++ DreamTeam.java
@@ -0,0 +1,29 @@
+/**
+ * Java Core. Homework 1.
+ *
+ * @author Elena Oxenhorn
+ * @version 17.10.2021
+ */
+
+public class DreamTeam {
+    public static void main(String[] args) {
+        Course course = new Course(1000, 1, 100);
+        Team[] teams = {
+            new Team("John", 1500, 3, 500, false),
+            new Team("Ilona", 1300, 2, 100, false),
+            new Team("Thomas", 500, 3, 200, false),
+            new Team("Alisa", 1000, 1, 50, false),
+        };
+        course.info();
+        System.out.println();
+        for (Team team : teams) {
+            System.out.print("Team " + team.getTeamName() + " , member: ");
+            System.out.println(team);
+        }
+        System.out.println();
+        for (Team team : teams) {
+            team.distancePassed(course);
+            team.info();
+        }
+    }
+}
